e4b9c42771f6be6ae5efac109889afc7bf279020
Galuh Akbar <galuh.akbar30@gmail.com>
2020-10-18 01:58:35 +0700

    ewcreh

--- resources/views/front/project/detail.blade.php
+++ resources/views/front/project/detail.blade.php
@@ -2,8 +2,121 @@
 
 @section('content')
 
+
 <div class="wrapper light-wrapper">
-    ini halaman project design
+    <div class="blog classic-view boxed">
+        <div class="post mb-0">
+          <div class="box bg-white shadow">
+
+            <figure class="main rounded"><img src="{{ asset('front/images/art/b13.jpg') }}" alt=""></figure>
+            <div class="space40"></div>
+            <div class="post-content">
+              <div class="category text-center"><a href="#" class="badge badge-pill bg-hibiscus">Concept</a></div>
+              <h2 class="post-title text-center">Ullamcorper Ridiculus Tristique Adipiscing</h2>
+              <div class="meta text-center"><span class="date"><i class="jam jam-clock"></i>5 Jul 2018</span><span class="author"><i class="jam jam-user"></i><a href="#">By Snowflake</a></span><span class="comments"><i class="jam jam-message-alt"></i><a href="#">3 Comments</a></span></div>
+              <p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur. Curabitur blandit tempus porttitor. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Nullam quis risus eget porta ac consectetur vestibulum.</p>
+              <p>Donec sed odio dui consectetur adipiscing elit. Etiam adipiscing tincidunt elit, eu convallis felis suscipit ut. Phasellus rhoncus tincidunt auctor. Nullam eu sagittis mauris. Donec non dolor ac elit aliquam tincidunt at at sapien. Aenean tortor libero, condimentum ac laoreet vitae, varius tempor nisi. Duis non arcu vel lectus urna mollis ornare vel eu leo.</p>
+              <div class="space30"></div>
+              <blockquote class="icon">
+                <div class="grid grid-view">
+                    <div class="tiles tiles-m light-gallery-wrapper">
+                      <div class="row isotope" style="position: relative; height: 1332.42px;">
+                        <div class="item grid-sizer col-md-6 corporate application aos-init aos-animate" data-aos="fade-up" style="position: absolute; left: 0%; top: 0px;">
+                          <figure class="overlay overlay2 rounded"><a href="{{asset('front/images/art/pp3-1-full.jpg')}}" class="lightbox"><span class="bg"></span> <img src="{{asset('front/images/art/pp3-1.jpg')}}"></a></figure>
+                        </div>
+                        <!-- /.item -->
+                        <div class="item grid-sizer col-md-6 corporate application aos-init aos-animate" data-aos="fade-up" style="position: absolute; left: 50%; top: 0px;">
+                          <figure class="overlay overlay2 rounded"><a href="{{asset('front/images/art/pp3-2-full.jpg')}}" class="lightbox"><span class="bg"></span> <img src="{{asset('front/images/art/pp3-2.jpg')}}"></a></figure>
+                        </div>
+                        <!-- /.item -->
+                        <div class="item grid-sizer col-md-6 corporate application aos-init aos-animate" data-aos="fade-up" style="position: absolute; left: 0%; top: 332.969px;">
+                          <figure class="overlay overlay2 rounded"><a href="{{asset('front/images/art/pp3-3-full.jpg')}}" class="lightbox"><span class="bg"></span> <img src="{{asset('front/images/art/pp3-3.jpg')}}"></a></figure>
+                        </div>
+                        <!-- /.item -->
+                        <div class="item grid-sizer col-md-6 corporate application aos-init aos-animate" data-aos="fade-up" style="position: absolute; left: 50%; top: 409.5px;">
+                          <figure class="overlay overlay2 rounded"><a href="{{asset('front/images/art/pp3-4-full.jpg')}}" class="lightbox"><span class="bg"></span> <img src="{{asset('front/images/art/pp3-4.jpg')}}"></a></figure>
+                        </div>
+                        <!-- /.item -->
+                        <div class="item grid-sizer col-md-6 corporate application aos-init aos-animate" data-aos="fade-up" style="position: absolute; left: 50%; top: 803.453px;">
+                          <figure class="overlay overlay2 rounded"><a href="{{asset('front/images/art/pp3-5-full.jpg')}}" class="lightbox"><span class="bg"></span> <img src="{{asset('front/images/art/pp3-5.jpg')}}"></a></figure>
+                        </div>
+                        <!-- /.item -->
+                        <div class="item grid-sizer col-md-6 corporate application aos-init aos-animate" data-aos="fade-up" style="position: absolute; left: 0%; top: 938.469px;">
+                          <figure class="overlay overlay2 rounded"><a href="{{asset('front/images/art/pp3-6-full.jpg')}}" class="lightbox"><span class="bg"></span> <img src="{{asset('front/images/art/pp3-6.jpg')}}"></a></figure>
+                        </div>
+                        <!-- /.item -->
+                      </div>
+                      <!-- /.row -->
+                    </div>
+                    <!-- /.tiles -->
+                  </div>
+                  <div class="row">
+                    <div class="col-lg-10 offset-lg-1">
+                      <h3 class="mb-30">You Might Also Like</h3>
+                      <div class="grid-view">
+                        <div class="carousel owl-carousel owl-loaded owl-drag" data-margin="30" data-dots="true" data-autoplay="false" data-autoplay-timeout="5000" data-responsive="{&quot;0&quot;:{&quot;items&quot;: &quot;1&quot;}, &quot;768&quot;:{&quot;items&quot;: &quot;2&quot;}, &quot;992&quot;:{&quot;items&quot;: &quot;3&quot;}}">
+
+                          <!-- /.item -->
+
+                          <!-- /.item -->
+
+                          <!-- /.item -->
+
+                          <!-- /.item -->
+
+                          <!-- /.item -->
+                        <div class="owl-stage-outer"><div class="owl-stage" style="transform: translate3d(0px, 0px, 0px); transition: all 0.25s ease 0s; width: 1625px;"><div class="owl-item active" style="width: 295px; margin-right: 30px;"><div class="item">
+                            <figure class="overlay overlay1 rounded mb-30"><a href="#"><span class="bg"></span> <img src="{{asset('front/images/art/b1.jpg')}}" alt=""></a>
+                              <figcaption>
+                                <h5 class="from-top mb-0">Read More</h5>
+                              </figcaption>
+                            </figure>
+                            <div class="category"><a href="#" class="badge badge-pill bg-purple">Concept</a></div>
+                            <h2 class="post-title"><a href="blog-post.html">Ligula tristique quis risus eget urna mollis ornare porttitor</a></h2>
+                            <div class="meta mb-0"><span class="date"><i class="jam jam-clock"></i>5 Jul 2018</span><span class="comments"><i class="jam jam-message-alt"></i><a href="#">3 Comments</a></span></div>
+                          </div></div><div class="owl-item active" style="width: 295px; margin-right: 30px;"><div class="item">
+                            <figure class="overlay overlay1 rounded mb-30"><a href="#"><span class="bg"></span> <img src="{{asset('front/images/art/b2.jpg')}}" alt=""></a>
+                              <figcaption>
+                                <h5 class="from-top mb-0">Read More</h5>
+                              </figcaption>
+                            </figure>
+                            <div class="category"><a href="#" class="badge badge-pill bg-meander">Business</a></div>
+                            <h2 class="post-title"><a href="blog-post.html">Nullam id dolor elit id nibh pharetra augue venenatis</a></h2>
+                            <div class="meta mb-0"><span class="date"><i class="jam jam-clock"></i>18 Jun 2018</span><span class="comments"><i class="jam jam-message-alt"></i><a href="#">5 Comments</a></span></div>
+                          </div></div><div class="owl-item active" style="width: 295px; margin-right: 30px;"><div class="item">
+                            <figure class="overlay overlay1 rounded mb-30"><a href="#"><span class="bg"></span> <img src="{{asset('front/images/art/b3.jpg')}}" alt=""></a>
+                              <figcaption>
+                                <h5 class="from-top mb-0">Read More</h5>
+                              </figcaption>
+                            </figure>
+                            <div class="category"><a href="#" class="badge badge-pill bg-pink">Design</a></div>
+                            <h2 class="post-title"><a href="blog-post.html">Ultricies fusce porta elit pharetra augue faucibus</a></h2>
+                            <div class="meta mb-0"><span class="date"><i class="jam jam-clock"></i>14 May 2018</span><span class="comments"><i class="jam jam-message-alt"></i><a href="#">7 Comments</a></span></div>
+                          </div></div><div class="owl-item" style="width: 295px; margin-right: 30px;"><div class="item">
+                            <figure class="overlay overlay1 rounded mb-30"><a href="#"><span class="bg"></span> <img src="{{asset('front/images/art/b4.jpg')}}" alt=""></a>
+                              <figcaption>
+                                <h5 class="from-top mb-0">Read More</h5>
+                              </figcaption>
+                            </figure>
+                            <div class="category"><a href="#" class="badge badge-pill bg-violet">Ideas</a></div>
+                            <h2 class="post-title"><a href="blog-post.html">Morbi leo risus porta eget metus est non commodolacus</a></h2>
+                            <div class="meta mb-0"><span class="date"><i class="jam jam-clock"></i>9 Apr 2018</span><span class="comments"><i class="jam jam-message-alt"></i><a href="#">4 Comments</a></span></div>
+                          </div></div><div class="owl-item" style="width: 295px; margin-right: 30px;"><div class="item">
+                            <figure class="overlay overlay1 rounded mb-30"><a href="#"><span class="bg"></span> <img src="{{asset('front/images/art/b5.jpg')}}" alt=""></a>
+                              <figcaption>
+                                <h5 class="from-top mb-0">Read More</h5>
+                              </figcaption>
+                            </figure>
+                            <div class="category"><a href="#" class="badge badge-pill bg-green">Workspace</a></div>
+                            <h2 class="post-title"><a href="blog-post.html">Mollis adipiscing lorem quis mollis eget lacinia faucibus</a></h2>
+                            <div class="meta mb-0"><span class="date"><i class="jam jam-clock"></i>23 Feb 2018</span><span class="comments"><i class="jam jam-message-alt"></i><a href="#">8 Comments</a></span></div>
+                          </div></div></div></div><div class="owl-nav disabled"><button type="button" role="presentation" class="owl-prev"><span aria-label="Previous">‹</span></button><button type="button" role="presentation" class="owl-next"><span aria-label="Next">›</span></button></div><div class="owl-dots"><button role="button" class="owl-dot active"><span></span></button><button role="button" class="owl-dot"><span></span></button><button role="button" class="owl-dot"><span></span></button></div></div>
+                        <!-- /.owl-carousel -->
+                      </div>
+                      <!-- /.grid-view -->
+                    </div>
+                    <!-- /column -->
+                  </div>
 </div>
 
 @endsection
